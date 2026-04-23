868c7b1c4d45c6a5b93447843d93edd15c069ac3
pranjalvatsa <pranjalvatsa86@gmail.com>
2026-04-23 13:38:05 +0530

    Tune typing speed for better demo pacing

--- src/utils/cli-effects.ts
+++ src/utils/cli-effects.ts
@@ -2,7 +2,7 @@ export async function sleep(ms: number) {
   return new Promise(res => setTimeout(res, ms));
 }
 
-export async function typeText(text: string, delay = 5) {
+export async function typeText(text: string, delay = 8) {
   for (const char of text) {
     process.stdout.write(char);
     await new Promise(res => setTimeout(res, delay));
